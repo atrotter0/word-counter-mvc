029d576c5ac9b5d2c6793b744f4a44e241b15e76
Abel Trotter <a_trotter0@hotmail.com>
2018-07-06 14:29:21 -0700

    Add front-end js validation

--- WordCounterProject/wwwroot/js/scripts.js
+++ WordCounterProject/wwwroot/js/scripts.js
@@ -1,16 +1,49 @@
-function validateInput() {
-  var userWord = $("#word-input").val();
-  var userPhrase = $("#phrase-input").val();
+var notBlank = false;
+var fieldValid = false;
 
-  if (userWord !== "" && userPhrase !== "") {
-    getWordCount(userWord, userPhrase);
+function onlyLetters(element) {
+  if ($(element).val().match(/[^a-zA-Z]/)) {
+    var msg = '* Only letters are allowed.';
+    displayInputError(element, msg);
+    fieldValid = false;
   } else {
-    displayError();
+    resetInputError(element);
+    fieldValid = true;
   }
 }
 
-function displayError() {
-  $(".error-message").css("visibility", "visible").text("Invalid input");
+function displayInputError(element, msg) {
+  $(element).css('border', '1px solid red');
+  $('.error-display').css('visibility', 'visible').text(msg);
+}
+
+function resetInputError(element) {
+  $(element).css('border', '1px solid #ccc');
+  $('.error-display').css('visibility', 'hidden').text('');
+}
+
+function checkFormFields() {
+  if ($('#word-input').val() !== "" && $('#phrase-input').val() !== "") {
+    notBlank = true;
+  } else {
+    notBlank = false;
+  }
+}
+
+function checkInputFlags() {
+  if (notBlank && fieldValid) {
+    enableBtn('#count-word');
+  } else {
+    disableBtn('#count-word');
+  }
+}
+
+function disableBtn(element) {
+  $(element).prop('disabled', 'true').css('border', '1px solid #ccc');
+}
+
+function enableBtn(element) {
+  $(element).removeAttr('disabled').css('border', '1px solid #00000061');
 }
 
 function getWordCount(word, phrase) {
@@ -19,7 +52,6 @@ function getWordCount(word, phrase) {
     data: { word: word, phrase: phrase},
     url: '/word-counter/' + word + '/' + phrase + '/',
     success: function(result) {
-      console.log("Success!");
       displayResult('.jumbotron', '.container', result);
     },
     error: function(err) {
@@ -34,9 +66,24 @@ function displayResult(removeElement, appendTo, result) {
 }
 
 $(document).ready(function() {
-  $("#count-word").click(function(e) {
+  disableBtn('#count-word');
+
+  $('#word-input').keyup(function() {
+    onlyLetters(this);
+    checkFormFields();
+    checkInputFlags();
+  });
+
+  $('#phrase-input').keyup(function() {
+    checkFormFields();
+    checkInputFlags();
+  });
+
+  $('#count-word').click(function(e) {
     e.preventDefault();
 
-    validateInput();
+    var userWord = $('#word-input').val();
+    var userPhrase = $('#phrase-input').val();
+    getWordCount(userWord, userPhrase);
   });
 });
